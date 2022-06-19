df7f76dbcf05b69692ea19057a4725db42bd867b
Jiho Lee <optional.int@kakao.com>
2022-06-19 14:57:17 +0000

    feat: separate inference folder

--- builder/run.sh
+++ builder/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-cd /usr/src/git-repo/builder
+cd /usr/src/git-repo/inference
 
 kubectl delete -f deployment.yaml --wait
 
 cd /usr/src/build-repo
 ./dockerize.sh
 
-cd /usr/src/git-repo/builder
+cd /usr/src/git-repo/inference
 
 kubectl apply -f deployment.yaml --wait
