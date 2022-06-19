c512e3f4050b8b765c0cb1fced9ef182fd5ccfaa
jiho lee <optional.int@kakao.com>
2022-06-19 15:32:14 +0900

    feat: init ModelRepository

--- api/src/main/java/io/so1s/Poc/domain/model/repository/ModelRepository.java
+++ api/src/main/java/io/so1s/Poc/domain/model/repository/ModelRepository.java
@@ -1,8 +1,11 @@
 package io.so1s.Poc.domain.model.repository;
 
+import io.so1s.Poc.domain.model.entity.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModelRepository {
+public interface ModelRepository extends JpaRepository<Model, Long> {
+
 
 }
