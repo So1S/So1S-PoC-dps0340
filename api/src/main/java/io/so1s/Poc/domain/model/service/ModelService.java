3e75256eb2d88b9ea63c1741cae830650f4ca0d9
jiho lee <optional.int@kakao.com>
2022-06-19 15:33:22 +0900

    feat: implement cloneGitRepository

--- api/src/main/java/io/so1s/Poc/domain/model/service/ModelService.java
+++ api/src/main/java/io/so1s/Poc/domain/model/service/ModelService.java
@@ -1,8 +1,39 @@
 package io.so1s.Poc.domain.model.service;
 
+import io.so1s.Poc.domain.model.entity.Model;
+import io.so1s.Poc.domain.model.entity.ModelType;
+import io.so1s.Poc.domain.model.repository.ModelRepository;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 @Service
+@RequiredArgsConstructor
 public class ModelService {
+    private final ModelRepository modelRepository;
+
+    public Model cloneGitRepository(String url) throws GitAPIException, IOException {
+        File tempFolder = Files.createTempDirectory("tempGit").toFile();
+
+        Git git = Git.cloneRepository()
+                .setURI(url)
+                .setDirectory(tempFolder)
+                .call();
+
+
+        Model model = modelRepository.saveAndFlush(
+                Model.builder()
+                        .url(url)
+                        .modelType(ModelType.GIT)
+                        .build()
+        );
+
+        return model;
+    }
 
 }
