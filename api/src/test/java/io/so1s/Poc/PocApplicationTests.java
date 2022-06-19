201c6809f46cec624bc523c193ee1dbc74add1ca
jiho lee <optional.int@kakao.com>
2022-06-19 12:45:14 +0900

    feat: init api server

---
+++ api/src/test/java/io/so1s/Poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.so1s.Poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
